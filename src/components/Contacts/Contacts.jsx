d9f68a83acb1085afd6b6107e27cc480e8f2698d
Olena Bondar <olena.bondar1060@gmail.com>
2023-02-22 19:41:54 +0200

    create Contacts

---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+
+import { getFilteredContacts } from 'redux/selectors';
+
+import ContactItem from 'components/ContactItem/ContactItem';
+import { List } from './Contacts.styled';
+
+export const Contacts = () => {
+  const contacts = useSelector(getFilteredContacts);
+
+  return (
+    <List>
+      {contacts.map(({ id, name, number }) => (
+        <ContactItem key={id} id={id} name={name} number={number} />
+      ))}
+    </List>
+  );
+};
+export default Contacts;
