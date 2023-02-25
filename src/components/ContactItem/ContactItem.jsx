b43e8f0ab7676ebb9b1c709cc8bd0385168539c1
Olena Bondar <olena.bondar1060@gmail.com>
2023-02-25 12:48:11 +0200

    Update ContactItem.jsx

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { deleteContact } from 'redux/actions';
+import { deleteContact } from 'redux/contacts/contacts-slice';
 import { Item, Button } from './ContactItem.styled';
 
 const ContactItem = ({ id, name, number }) => {
